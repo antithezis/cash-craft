71fc3a14dfe93bfcfd40d78ef19d28bc49a0d4e8
antithezis <juandbm2001@gmail.com>
2024-03-28 01:26:51 -0500

    Update routes configuration

--- cash-craft-frontend/src/app/app.routes.ts
+++ cash-craft-frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.routes').then(r => r.AUTH_ROUTES)
+    loadChildren: () => import('./auth/auth.routes').then((r) => r.AUTH_ROUTES),
   },
   {
-    path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.routes').then(r => r.DASHBOARD_ROUTES)
-  }
+    path: '',
+    loadChildren: () =>
+      import('./dashboard/dashboard.routes').then((r) => r.DASHBOARD_ROUTES),
+  },
+  { path: '**', redirectTo: '' },
 ];
